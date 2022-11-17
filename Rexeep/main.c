8ba72449394dd352251eb4493b892c69c4d06b10
m.marian <mahdi.maryan@yahoo.com>
2022-11-17 16:17:51 +0330

    on last set

--- Rexeep/main.c
+++ Rexeep/main.c
@@ -6,14 +6,28 @@
 
 #include "Stack.h"
 
+#define LOGF 0
+
 int main() {
-	char* rules[5] = { "S=ACB|Cbb|Ba", "B=da|BC", "C=g|#", "D=h|#" };
+	char* rules[7] = { "S=AkO", "A=aZ", "Z=BX|CX", "C=c", "B=bBC|r", "X=dX|#" };
 
 	fl_tree* tree = fl_instance();
 	create_fl_tree(tree, rules);
 	ls_compute(tree, rules);
 
-	ls_node* noode = tree->last_table->get(tree->last_table, "B");
+	ls_node* noode = tree->last_table->get(tree->last_table, "Z");
+
+	if (LOGF) {
+		List* fss = list_instance();
+		get_first_set(tree, "D", fss);
+
+		for (size_t i = 0; i < fss->count; i++)
+		{
+			printf(fss->tape[i]);
+		}
+
+		return 0;
+	}
 
 	for (size_t i = 0; i < noode->terminals->count; i++)
 	{
